0c048ec5e93bdd0e48004cd7fcc416f57da241a1
Airels <yohan13240@hotmail.fr>
2022-03-19 22:32:31 +0100

    Group possède nom unique

--- src/main/java/fr/univamu/annuaire/model/beans/Group.java
+++ src/main/java/fr/univamu/annuaire/model/beans/Group.java
@@ -24,6 +24,7 @@ public class Group implements Serializable {
     private long id;
 
     @Basic
+    @Column(nullable = false, unique = true)
     private String name;
 
     public Group(String name) {
