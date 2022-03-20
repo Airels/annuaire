7093683b2d3d552e42584c921a8ee72e1fb8bc6b
Airels <yohan13240@hotmail.fr>
2022-03-20 19:59:04 +0100

    Tests DefaultUpdaterService à implémenter

---
+++ src/test/java/fr/univamu/annuaire/model/services/updater/DefaultUpdaterServiceTest.java
@@ -0,0 +1,35 @@
+package fr.univamu.annuaire.model.services.updater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+// TODO
+class DefaultUpdaterServiceTest {
+
+    @Test
+    void addPerson() {
+    }
+
+    @Test
+    void addGroup() {
+    }
+
+    @Test
+    void updatePerson() {
+    }
+
+    @Test
+    void updateGroup() {
+    }
+
+    @Test
+    void removePerson() {
+    }
+
+    @Test
+    void removeGroup() {
+    }
+}
