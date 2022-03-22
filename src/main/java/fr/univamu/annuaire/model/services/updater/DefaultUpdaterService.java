067aa35a13921529583e43bf7172ee94ecbe781e
Airels <yohan13240@hotmail.fr>
2022-03-22 10:20:48 +0100

    Implémentation UpdaterService + tests

--- src/main/java/fr/univamu/annuaire/model/services/updater/DefaultUpdaterService.java
+++ src/main/java/fr/univamu/annuaire/model/services/updater/DefaultUpdaterService.java
@@ -1,7 +1,9 @@
 package fr.univamu.annuaire.model.services.updater;
 
 import fr.univamu.annuaire.exceptions.GroupAlreadyExistException;
+import fr.univamu.annuaire.exceptions.GroupNotExistException;
 import fr.univamu.annuaire.exceptions.PersonAlreadyExistException;
+import fr.univamu.annuaire.exceptions.PersonNotExistException;
 import fr.univamu.annuaire.model.beans.Group;
 import fr.univamu.annuaire.model.beans.Person;
 import fr.univamu.annuaire.model.repository.GroupRepository;
@@ -10,6 +12,9 @@ import fr.univamu.annuaire.model.services.settlement.SettlementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 @Service("defaultUpdaterService")
 @Primary
@@ -26,31 +31,49 @@ public class DefaultUpdaterService implements UpdaterService {
     public void addPerson(Person p) throws Exception {
         Person person = personRepository.findByEmail(p.getEmail());
         if (person != null) throw new PersonAlreadyExistException(p);
+        personRepository.save(p);
     }
 
     @Override
     public void addGroup(Group g) throws Exception {
         Group group = groupRepository.findByName(g.getName());
         if (group != null) throw new GroupAlreadyExistException(g);
+
+        groupRepository.save(g);
     }
 
     @Override
     public void updatePerson(Person p) throws Exception {
+        Person person = personRepository.findByEmail(p.getEmail());
+        if (person == null) throw new PersonNotExistException(p);
+
         personRepository.save(p);
     }
 
     @Override
     public void updateGroup(Group g) throws Exception {
+        Optional<Group> group = groupRepository.findById(g.getId());
+        if (group.isEmpty()) throw new GroupNotExistException(g);
+
+        Group group2 = groupRepository.findByName(g.getName());
+        if (group2 != null) throw new GroupAlreadyExistException(g);
+
         groupRepository.save(g);
     }
 
     @Override
     public void removePerson(Person p) throws Exception {
+        Person person = personRepository.findByEmail(p.getEmail());
+        if (person == null) throw new PersonNotExistException(p);
+
         personRepository.delete(p);
     }
 
     @Override
     public void removeGroup(Group g) throws Exception {
+        Group group = groupRepository.findByName(g.getName());
+        if (group == null) throw new GroupNotExistException(g);
+
         groupRepository.delete(g);
     }
 }
--- src/test/java/fr/univamu/annuaire/model/services/updater/DefaultUpdaterServiceTest.java
+++ src/test/java/fr/univamu/annuaire/model/services/updater/DefaultUpdaterServiceTest.java
@@ -1,35 +1,103 @@
 package fr.univamu.annuaire.model.services.updater;
 
+import fr.univamu.annuaire.exceptions.GroupAlreadyExistException;
+import fr.univamu.annuaire.exceptions.GroupNotExistException;
+import fr.univamu.annuaire.exceptions.PersonAlreadyExistException;
+import fr.univamu.annuaire.exceptions.PersonNotExistException;
+import fr.univamu.annuaire.model.beans.Group;
+import fr.univamu.annuaire.model.beans.Person;
+import fr.univamu.annuaire.model.repository.GroupRepository;
+import fr.univamu.annuaire.model.repository.PersonRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
-// TODO
 class DefaultUpdaterServiceTest {
 
-    @Test
-    void addPerson() {
+    @Autowired
+    @Qualifier("defaultUpdaterService")
+    UpdaterService updaterService;
+
+    @Autowired
+    PersonRepository personRepository;
+
+    @Autowired
+    GroupRepository groupRepository;
+
+    private Person defaultPerson;
+    private Group defaultGroup;
+
+    @BeforeEach
+    void beforeEach() throws Exception {
+        defaultPerson = new Person("before", "each", "before@each.com", "passwd");
+        personRepository.save(defaultPerson);
+
+        defaultGroup = new Group("before-each");
+        groupRepository.save(defaultGroup);
+    }
+
+    @AfterEach
+    void afterEach() throws Exception {
+        personRepository.delete(defaultPerson);
+        groupRepository.delete(defaultGroup);
     }
 
     @Test
-    void addGroup() {
+    void addPerson() throws Exception {
+        Person p = new Person("first", "last", "a@b.com", "passwd");
+        updaterService.addPerson(p);
+        Assertions.assertThrows(PersonAlreadyExistException.class, () -> updaterService.addPerson(p));
     }
 
     @Test
-    void updatePerson() {
+    void addGroup() throws Exception {
+        Group g = new Group("grp");
+        updaterService.addGroup(g);
+        Assertions.assertThrows(GroupAlreadyExistException.class, () -> updaterService.addGroup(g));
     }
 
     @Test
-    void updateGroup() {
+    void updatePerson() throws Exception {
+        defaultPerson.setLastName("aaa");
+        updaterService.updatePerson(defaultPerson);
+        Assertions.assertEquals("aaa", personRepository.findByEmail(defaultPerson.getEmail()).getLastName());
+
+        Person p = new Person("first", "last", "a@b.com", "passwd");
+        Assertions.assertThrows(PersonNotExistException.class, () -> updaterService.updatePerson(p));
     }
 
     @Test
-    void removePerson() {
+    void updateGroup() throws Exception {
+        defaultGroup.setName("abc");
+        updaterService.updateGroup(defaultGroup);
+        Assertions.assertEquals("abc", groupRepository.findById(defaultGroup.getId()).get().getName());
+
+        Group g = new Group("grp");
+        Assertions.assertThrows(GroupNotExistException.class, () -> updaterService.updateGroup(g));
+
+        g.setId(defaultGroup.getId());
+        g.setName("abc");
+        Assertions.assertThrows(GroupAlreadyExistException.class, () -> updaterService.updateGroup(g));
     }
 
     @Test
-    void removeGroup() {
+    void removePerson() throws Exception {
+        updaterService.removePerson(defaultPerson);
+        Assertions.assertTrue(personRepository.findById(defaultPerson.getId()).isEmpty());
+
+        Assertions.assertThrows(PersonNotExistException.class, () -> updaterService.removePerson(defaultPerson));
+    }
+
+    @Test
+    void removeGroup() throws Exception {
+        updaterService.removeGroup(defaultGroup);
+        Assertions.assertTrue(groupRepository.findById(defaultGroup.getId()).isEmpty());
+
+        Assertions.assertThrows(GroupNotExistException.class, () -> updaterService.removeGroup(defaultGroup));
     }
 }
