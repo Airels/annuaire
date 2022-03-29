94bef7c6597be16a27ed27d1c47b347319ac1669
Airels <yohan13240@hotmail.fr>
2022-03-29 16:34:10 +0200

    Ajout annotations Repository + refactor

--- src/main/java/fr/univamu/annuaire/repository/GroupRepository.java
+++ src/main/java/fr/univamu/annuaire/repository/GroupRepository.java
@@ -3,9 +3,11 @@ package fr.univamu.annuaire.repository;
 import fr.univamu.annuaire.model.Group;
 import fr.univamu.annuaire.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface GroupRepository extends JpaRepository<Group, Long> {
 
     Group findByName(String name);
--- src/main/java/fr/univamu/annuaire/repository/PersonRepository.java
+++ src/main/java/fr/univamu/annuaire/repository/PersonRepository.java
@@ -2,11 +2,19 @@ package fr.univamu.annuaire.repository;
 
 import fr.univamu.annuaire.model.Group;
 import fr.univamu.annuaire.model.Person;
+import org.hibernate.Hibernate;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    Optional<Person> findById(Long id);
 
     List<Person> findByFirstName(String firstName);
 
@@ -17,6 +25,4 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
     List<Person> findByLastName(String lastName);
 
     List<Person> findByLastNameLike(String lastName);
-
-    List<Person> findByGroupsContaining(Group g);
 }
