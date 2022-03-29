ae84d82c6f1a990e78e57e1541b972c017222a8d
Airels <yohan13240@hotmail.fr>
2022-03-29 16:32:06 +0200

    Ajout sécurité spring

---
+++ src/main/java/fr/univamu/annuaire/security/SpringWebSecurity.java
@@ -0,0 +1,21 @@
+package fr.univamu.annuaire.security;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class SpringWebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/**")
+                .permitAll();
+    }
+}
