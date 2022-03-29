557dc9ebe3f88f78683483acb724314abb9da1f7
Airels <yohan13240@hotmail.fr>
2022-03-29 16:35:05 +0200

    Set up config Servlets

--- src/main/java/fr/univamu/annuaire/AnnuaireApplication.java
+++ src/main/java/fr/univamu/annuaire/AnnuaireApplication.java
@@ -2,12 +2,36 @@ package fr.univamu.annuaire;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 @SpringBootApplication
-public class AnnuaireApplication {
+public class AnnuaireApplication extends SpringBootServletInitializer implements WebMvcConfigurer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AnnuaireApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AnnuaireApplication.class, args);
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        System.out.println("--- addResourceHandlers");
+        registry.addResourceHandler("/webjars/**")//
+                .addResourceLocations("/webjars/");
+    }
 }
