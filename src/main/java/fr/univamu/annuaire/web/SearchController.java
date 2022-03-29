530302e2f82cb99a612ac3d33d5aae815c66b71f
Airels <yohan13240@hotmail.fr>
2022-03-29 18:14:51 +0200

    Ajout futur controller de recherche

---
+++ src/main/java/fr/univamu/annuaire/web/SearchController.java
@@ -0,0 +1,10 @@
+package fr.univamu.annuaire.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/search")
+public class SearchController {
+
+}
