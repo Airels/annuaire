8a1e92db904e74a69e56e29b1e2b2cc16abde14e
Airels <yohan13240@hotmail.fr>
2022-03-30 20:07:08 +0200

    Update groupRepository pour charger les personnes ET trouver un groupe par "like"

--- src/main/java/fr/univamu/annuaire/repository/GroupRepository.java
+++ src/main/java/fr/univamu/annuaire/repository/GroupRepository.java
@@ -2,15 +2,22 @@ package fr.univamu.annuaire.repository;
 
 import fr.univamu.annuaire.model.Group;
 import fr.univamu.annuaire.model.Person;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface GroupRepository extends JpaRepository<Group, Long> {
 
+    @EntityGraph(attributePaths = { "persons" })
+    Optional<Group> findById(Long id);
+
     Group findByName(String name);
+
+    List<Group> findByNameLike(String name);
 
     List<Group> findByPersonsContaining(Person p);
 }
