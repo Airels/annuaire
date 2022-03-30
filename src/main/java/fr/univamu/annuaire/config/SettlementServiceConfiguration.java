c10832c6ebe25c862081464ea784929e50c6d27c
Airels <yohan13240@hotmail.fr>
2022-03-30 20:10:24 +0200

    Suppression SettlementServiceConfiguration.java

--- src/main/java/fr/univamu/annuaire/config/SettlementServiceConfiguration.java
+++
@@ -1,9 +0,0 @@
-package fr.univamu.annuaire.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SettlementServiceConfiguration {
-
-}
