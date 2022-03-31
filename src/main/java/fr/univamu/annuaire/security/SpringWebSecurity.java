724d6933725cb502c0412517fcddde2ec6d2a099
Airels <yohan13240@hotmail.fr>
2022-03-31 16:48:10 +0200

    Ajout PasswordEncoder

--- src/main/java/fr/univamu/annuaire/security/SpringWebSecurity.java
+++ src/main/java/fr/univamu/annuaire/security/SpringWebSecurity.java
@@ -1,9 +1,12 @@
 package fr.univamu.annuaire.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -19,5 +22,10 @@ public class SpringWebSecurity extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and().formLogin().permitAll()
                 .and().logout().permitAll();
+    }
+
+    @Bean("passwordEncoder")
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
