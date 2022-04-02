171b40045271c33654a11d3b62fc21937f2f06b9
Airels <yohan13240@hotmail.fr>
2022-04-02 17:40:53 +0200

    Ajout d'une entité PersonUpdateFormModel utilisé pour le formulaire

---
+++ src/main/java/fr/univamu/annuaire/model/PersonUpdateFormModel.java
@@ -0,0 +1,41 @@
+package fr.univamu.annuaire.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
+@ToString(callSuper = true)
+public class PersonUpdateFormModel extends Person {
+
+    private String repeatPassword;
+
+    public PersonUpdateFormModel(Person person) {
+        super(
+                person.getId(),
+                person.getEmail(),
+                person.getPassword(),
+                person.getLastName(),
+                person.getFirstName(),
+                person.getBirthday(),
+                person.getWebsite(),
+                person.getRoles()
+        );
+    }
+
+    public Person toPerson() {
+        return new Person(
+                this.getId(),
+                this.getEmail(),
+                this.getPassword(),
+                this.getLastName(),
+                this.getFirstName(),
+                this.getBirthday(),
+                this.getWebsite(),
+                this.getRoles()
+        );
+    }
+}
