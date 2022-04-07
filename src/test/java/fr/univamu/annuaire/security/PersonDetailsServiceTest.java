e5b5a0340af250609f9c7079128e03d55e832055
Airels <yohan13240@hotmail.fr>
2022-04-07 16:11:44 +0200

    Ajout test

---
+++ src/test/java/fr/univamu/annuaire/security/PersonDetailsServiceTest.java
@@ -0,0 +1,43 @@
+package fr.univamu.annuaire.security;
+
+import fr.univamu.annuaire.model.Person;
+import fr.univamu.annuaire.repository.PersonRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PersonDetailsServiceTest {
+
+    @Autowired
+    PersonRepository personRepository;
+
+    @Autowired
+    PersonDetailsService personDetailsService;
+
+    Person p1;
+
+    @BeforeEach
+    void beforeEach() {
+        p1 = new Person("First", "Last", "test@email", "passwd");
+        personRepository.save(p1);
+    }
+
+    @AfterEach
+    void afterEach() {
+        personRepository.delete(p1);
+    }
+
+    @Test
+    void loadUserByUsername() {
+        PersonDetails p = (PersonDetails) personDetailsService.loadUserByUsername(p1.getEmail());
+        Assertions.assertEquals(p1.getId(), p.getPerson().getId());
+    }
+}
